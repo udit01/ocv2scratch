75b40b31318496cf3e9eeffc8cdd3fcb766a57fa
“udit01” <uditjainiitd@gmail.com>
2017-11-24 15:58:39 +0530

    making progress , 3 programs working

---
+++ version.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cv.h>
+
+int main(void)
+{
+    printf("%s\r\n", CV_VERSION);
+    printf("%u.%u.%u\r\n", CV_MAJOR_VERSION, CV_MINOR_VERSION, CV_SUBMINOR_VERSION);
+}
+
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
+// hellloooo
